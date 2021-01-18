75d7ee72c894713f6dd479a539f1de8a66f305b7
fern <felipe.ghidini@gft.com>
2021-01-18 14:40:40 -0300

    Service layer, component registration

---
+++ src/main/java/com/nelioalves/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.nelioalves.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.course.entities.User;
+import com.nelioalves.course.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> finAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get(); 
+	}
+
+}

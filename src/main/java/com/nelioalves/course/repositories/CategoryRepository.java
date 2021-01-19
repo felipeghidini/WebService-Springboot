e16ea72479db24003a5408b10bcb665e9d5cd7b4
fern <felipe.ghidini@gft.com>
2021-01-19 13:39:25 -0300

    Category

---
+++ src/main/java/com/nelioalves/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.nelioalves.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nelioalves.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
---
+++ src/main/java/com/nelioalves/course/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.nelioalves.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.course.entities.Category;
+import com.nelioalves.course.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> finAll(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		return obj.get(); 
+	}
+
+}

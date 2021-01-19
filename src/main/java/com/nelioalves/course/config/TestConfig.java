8d441ee5d4c0ee71f9b705ed144c1c2d6c199903
fern <felipe.ghidini@gft.com>
2021-01-19 14:56:25 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/nelioalves/course/config/TestConfig.java
+++ src/main/java/com/nelioalves/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.nelioalves.course.entities.Category;
 import com.nelioalves.course.entities.Order;
+import com.nelioalves.course.entities.OrderItem;
 import com.nelioalves.course.entities.Product;
 import com.nelioalves.course.entities.User;
 import com.nelioalves.course.entities.enums.OrderStatus;
 import com.nelioalves.course.repositories.CategoryRepository;
+import com.nelioalves.course.repositories.OrderItemRepository;
 import com.nelioalves.course.repositories.OrderRepository;
 import com.nelioalves.course.repositories.ProductRepository;
 import com.nelioalves.course.repositories.UserRepository;
@@ -34,6 +36,9 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -50,17 +55,6 @@ public class TestConfig implements CommandLineRunner{
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
-		
-		userRepository.saveAll(Arrays.asList(u1, u2));
-		
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
@@ -69,6 +63,24 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 	}
 	
---
+++ src/main/java/com/nelioalves/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.nelioalves.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nelioalves.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}

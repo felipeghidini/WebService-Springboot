1dfb85f05a326ae2a3a0a205f74bf8be5cd776ec
felipeghidini <lipeghidini18@gmail.com>
2021-01-23 06:03:43 -0300

    Exception handing, findById

--- src/main/java/com/nelioalves/course/services/UserService.java
+++ src/main/java/com/nelioalves/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.nelioalves.course.entities.User;
 import com.nelioalves.course.repositories.UserRepository;
+import com.nelioalves.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -23,7 +24,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get(); 
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); 
 	}
 	
 	public User insert(User obj) {

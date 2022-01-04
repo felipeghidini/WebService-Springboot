4c14c45715cb14d551686255127fd97dafcb2176
felipeghidini <lipeghidini18@gmail.com>
2022-01-03 22:19:22 -0300

    teste feature

--- src/main/java/com/nelioalves/course/entities/Category.java
+++ src/main/java/com/nelioalves/course/entities/Category.java
@@ -14,7 +14,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "tb_categoryy")
+@Table(name = "tb_category")
 public class Category implements Serializable{
 	
 	
